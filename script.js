06c6d6efe7fde03465c06468a4692da2be6a6f4e
Vitor Marques <vitormarques03@gmail.com>
2025-04-28 23:38:19 -0300

    Atualizar fluxo de pedido e layout de endereço

--- script.js
+++ script.js
@@ -199,19 +199,19 @@ function montarCardapio() {
         nome: "Pão Integral",
         preco: 8,
         imagem:
-          "https://www.google.com/imgres?q=P%C3%A3o%20integral&imgurl=https%3A%2F%2Fspecialepaes.com%2Fwp-content%2Fuploads%2F2020%2F01%2Fpao-integral-saudavel-blog-speciale-fortaleza.jpg&imgrefurl=https%3A%2F%2Fspecialepaes.com%2Fdicas-de-saude%2F7-motivos-para-comer-pao-integral-speciale%2F&docid=1XpJNsQLKB9xWM&tbnid=tVY542keI6_HBM&vet=12ahUKEwiBsfzsk_yMAxW2jpUCHRLSFkEQM3oFCIEBEAA..i&w=1000&h=768&hcb=2&ved=2ahUKEwiBsfzsk_yMAxW2jpUCHRLSFkEQM3oFCIEBEAA"
+          "imagens/pao.jpg"
       },
       {
         nome: "Iogurte Natural",
         preco: 12,
         imagem:
-          "https://www.google.com/imgres?q=iogurte%20natural&imgurl=https%3A%2F%2Fminhasreceitinhas.com.br%2Fwp-content%2Fuploads%2F2022%2F11%2FIogurte-natural-caseiro-sabor-na-mesa-1200x847.jpg&imgrefurl=https%3A%2F%2Fminhasreceitinhas.com.br%2Freceita%2Fiogurte-natural-caseiro%2F&docid=1Y248trZxC9yiM&tbnid=u3XA6UHjQnJ5DM&vet=12ahUKEwjn4LP7k_yMAxUAqpUCHbFQJdsQM3oECEUQAA..i&w=1200&h=847&hcb=2&ved=2ahUKEwjn4LP7k_yMAxUAqpUCHbFQJdsQM3oECEUQAA"
+          "https://www.imagens/iogurte.jpg"
       },
       {
         nome: "Vitamina de Frutas",
         preco: 10,
         imagem:
-          "https://www.google.com/imgres?q=vitamina%20de%20frutas&imgurl=https%3A%2F%2Fvitat.com.br%2Freceitas%2Fimages%2Frecipeshandler.jpg%3Fid%3D7203%26tipo%3Dr%26default%3Ds%26ims%3Dfit-in%2F414x275%2Ffilters%3Aquality(60)&imgrefurl=https%3A%2F%2Fvitat.com.br%2Freceitas%2F7203-vitamina-de-frutas-vermelhas&docid=69KvqEfCsiewnM&tbnid=x7bmY8Uldd6QLM&vet=12ahUKEwjMs46ElPyMAxXEppUCHf8DHh4QM3oECH8QAA..i&w=393&h=275&hcb=2&ved=2ahUKEwjMs46ElPyMAxXEppUCHf8DHh4QM3oECH8QAA"
+          "imagens/vitamina.jpg"
       }
     ];
   } else if (refeicaoAtual === "almoco") {
@@ -220,19 +220,19 @@ function montarCardapio() {
         nome: "Frango Grelhado",
         preco: 35,
         imagem:
-          "https://www.google.com/imgres?q=frango%20grelhado&imgurl=https%3A%2F%2Fimg.cybercook.com.br%2Fimagens%2Freceitas%2F340%2Ffile-de-frango-grelhado-2.jpeg&imgrefurl=https%3A%2F%2Fcybercook.com.br%2Freceitas%2Faves%2Ffile-de-frango-grelhado-85340&docid=2kKqCFsuOskQrM&tbnid=lkI101LkTFi8LM&vet=12ahUKEwiuw-uZlPyMAxWlp5UCHQZRFQQQM3oECBsQAA..i&w=2000&h=1344&hcb=2&ved=2ahUKEwiuw-uZlPyMAxWlp5UCHQZRFQQQM3oECBsQAAg"
+          "imagens/frango.jpg"
       },
       {
         nome: "Salada de Frutas",
         preco: 20,
         imagem:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tudogostoso.com.br%2Freceita%2F787-salada-de-frutas-gostosa.html&psig=AOvVaw2qB_NZ6GXwGpbeJhwyRVoF&ust=1745978867351000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLCombCU_IwDFQAAAAAdAAAAABAE"
+          "imagens/salada.jpg"
       },
       {
         nome: "Tapioca",
         preco: 10,
         imagem:
-          "hhttps://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.daterrinhaalimentos.com%2Fproduto%2Ftapioca-fortificada%2Ftapioca-fortificada-betacaroteno-240g&psig=AOvVaw0AKYQOcKBhMmc2NagTOMFV&ust=1745978908077000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNj8hcOU_IwDFQAAAAAdAAAAABAE"
+          "imagens/tapioca.jpg"
       }
     ];
   } else if (refeicaoAtual === "jantar") {
@@ -241,19 +241,19 @@ function montarCardapio() {
         nome: "Sopa de Legumes",
         preco: 18,
         imagem:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.receitasnestle.com.br%2Freceitas%2Fsopa-de-legumes&psig=AOvVaw0dyYyXOpaP0sJWTKdH9Pnk&ust=1745978926239000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiTuc2U_IwDFQAAAAAdAAAAABAE"
+          "imagens/sopa.jpg"
       },
       {
         nome: "Omelete Saudável",
         preco: 16,
         imagem:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Freceitasmondial.com.br%2Fblog%2Fcomo-fazer-a-omelete-perfeita%2F&psig=AOvVaw3A1KCtw_bhCp1tduXG45zg&ust=1745978948908000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCx7dWU_IwDFQAAAAAdAAAAABAE"
+          "imagens/omelete.jpg"
       },
       {
         nome: "Wrap Integral",
         preco: 22,
         imagem:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kitchenaid.com.br%2Fblog%2Freceitas%2Freceitas-salgadas%2Fwrap-integral&psig=AOvVaw3tk9fx3dWiTe4dl6qt-x_m&ust=1745978967365000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDj0OCU_IwDFQAAAAAdAAAAABAE"
+          "imagens/wrap.jpg"
       }
     ];
   }
